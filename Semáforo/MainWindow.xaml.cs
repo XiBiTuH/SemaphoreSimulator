22274dace3590ad12ca5ce526d7591e956a335d5
Pedro Brito <jorge.brito@ubi.pt>
2020-11-18 17:31:21 +0000

    test

--- Semáforo/MainWindow.xaml.cs
+++ Semáforo/MainWindow.xaml.cs
@@ -34,17 +34,52 @@ namespace Semáforo
             public static extern void SetDigitalChannel(int Channel);
             [DllImport("K8055D.dll")]
             public static extern bool ReadDigitalChannel(int Channel);
+            [DllImport("K8055D.dll")]
+            public static extern void ClearDigitalChannel(int Channel);
+            [DllImport("K8055D.dll")]
+            public static extern void SetAllDigital();
+            [DllImport("K8055D.dll")]
+            public static extern void WriteAllDigital(int number);
+            [DllImport("K8055D.dll")]
+            public static extern void ClearAllDigital();
         }
 
         public MainWindow()
         {
-            //InitializeComponent();
-            K8055.OpenDevice(0); //Open communication with K8055 that has the device address 0
-            K8055.SetDigitalChannel(1); //Sets digital output channel 1 to 'ON'
-            K8055.SetDigitalChannel(6); //Sets digital output channel 6 to 'ON'
-            Console.WriteLine(K8055.ReadDigitalChannel(2)); //Reads digital input channel and prints in console
-            //K8055.CloseDevice(); //Closes communication with the K8055
+            InitializeComponent();
+
         }
 
+
+        //Normal Mode
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            K8055.OpenDevice(0); //Open communication with K8055 that has the device address 0
+        }
+
+
+        // Automatic Mode
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            K8055.SetDigitalChannel(1); //Sets digital output channel 1 to 'ON'
+
+        }
+
+
+        //Peões
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        //Policia
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            K8055.ClearAllDigital();
+            K8055.SetDigitalChannel(2);
+            K8055.SetDigitalChannel(5);
+        }
     }
 }
